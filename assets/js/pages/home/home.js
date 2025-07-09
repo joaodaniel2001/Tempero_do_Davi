9c4059465a84728df3c1ebc332bf3f69097a9cc3
joaodaniel2001 <joaodaniel2al@gmail.com>
2025-07-09 12:11:53 -0300

    09.07.2025

--- assets/js/pages/home/home.js
+++ assets/js/pages/home/home.js
@@ -1,12 +1,13 @@
 
-// MENU
+// MENU, BANNER & LOGO
 
 let lastScroll = 0;
 const logo = document.getElementById("logo")
 
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
-    const header = document.querySelector("header")
+    const header = document.querySelector("#header")
+    const banner = document.querySelector("#banner")
 
     if (currentScroll === 0) {
         header.style.top = "0"
@@ -22,19 +23,39 @@ window.addEventListener("scroll", () => {
         logo.src = "./assets/img/global/logoLojaBranca.png"
     }
 
+    banner?.classList.toggle("rolagem", currentScroll > 0)
     lastScroll = currentScroll
 })
 
 // ICONES
 
-const cart = document.getElementById ("cart")
+const cart = document.getElementById("cart")
 
 // Carrinho vazio
-cart.addEventListener ("mouseover", () => {
-    cart.classList.replace ("bi-cart", "bi-cart-fill")
+cart.addEventListener("mouseover", () => {
+    cart.classList.replace("bi-cart", "bi-cart-fill")
 })
 
 // Carrinho cheio
-cart.addEventListener ("mouseout", () => {
-    cart.classList.replace ("bi-cart-fill", "bi-cart")
+cart.addEventListener("mouseout", () => {
+    cart.classList.replace("bi-cart-fill", "bi-cart")
 })
+
+// Roleta de IMG's
+
+const imagens = [
+    "./assets/img/pages/home/davi-img01.jpg",
+    "./assets/img/pages/home/davi-img02.jpg",
+    "./assets/img/pages/home/davi-img03.jpg",
+    "./assets/img/pages/home/davi-img04.jpg"
+]
+
+const imagem =  document.getElementById("imgCafeBresa")
+let index = 0
+
+setInterval (() => {
+    index = (index + 1) %imagens.length
+    if (imagem) {
+        imagem.src = imagens[index]
+    }
+}, 5000)
